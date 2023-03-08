b348c9b50d1df4772c386110dac65c0dabd9d46a
dunyanong <ong.dy2004@gmail.com>
2023-03-08 09:44:20 +0800

    first commit

---
+++ lib/notes.dart
@@ -0,0 +1,45 @@
+/*
+------------------------------------------------------------------------------------------------------------------------------------------
+Stateful Widget
+------------------------------------------------------------------------------------------------------------------------------------------
+
+class Test extends StatefulWidget {
+  const Test({super.key});
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+// State object
+class _TestState extends State<Test> {
+  int counter = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
